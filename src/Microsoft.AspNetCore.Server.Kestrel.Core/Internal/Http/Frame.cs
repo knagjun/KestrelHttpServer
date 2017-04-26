c9f2d6b7459b4f11d9e614474056dd43922fff3a
Cesar Blum Silveira <cesars@microsoft.com>
2017-04-25 20:10:30 -0700

    Trying more possible optimizations.

--- src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/Http/Frame.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/Http/Frame.cs
@@ -51,6 +51,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
 
         private Streams _frameStreams;
         protected readonly RequestBodyReader _requestBodyReader;
+        protected readonly IPipe _requestBodyPipe;
 
         protected Stack<KeyValuePair<Func<object, Task>, object>> _onStarting;
         protected Stack<KeyValuePair<Func<object, Task>, object>> _onCompleted;
@@ -63,6 +64,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
         protected RequestProcessingStatus _requestProcessingStatus;
         protected bool _keepAlive;
         protected bool _upgrade;
+        protected bool _hasRequestBody;
         private bool _canHaveBody;
         private bool _autoChunk;
         protected Exception _applicationException;
@@ -98,7 +100,8 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
             _keepAliveTicks = ServerOptions.Limits.KeepAliveTimeout.Ticks;
             _requestHeadersTimeoutTicks = ServerOptions.Limits.RequestHeadersTimeout.Ticks;
 
-            _requestBodyReader = new RequestBodyReader(CreateRequestBodyPipe());
+            _requestBodyPipe = CreateRequestBodyPipe();
+            _requestBodyReader = new RequestBodyReader(_requestBodyPipe);
         }
 
         public ServiceContext ServiceContext => _frameContext.ServiceContext;
@@ -322,6 +325,8 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
 
             _requestProcessingStatus = RequestProcessingStatus.RequestPending;
             _keepAlive = false;
+            _upgrade = false;
+            _hasRequestBody = false;
             _autoChunk = false;
             _applicationException = null;
 
